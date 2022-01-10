e05dd2c225e6cd739e55cc9aa9d35d5f7672d254
Royalboe <auomidejohnson@gmail.com>
2022-01-10 15:08:42 +0100

    Final Stage

--- Topics/Properties accessors/Weather comparison/src/Main.kt
+++ Topics/Properties accessors/Weather comparison/src/Main.kt
@@ -1,24 +1,6 @@
 class City(val name: String) {
-    var degrees: Int = 0
-        set(value) {
-            field = when {
-                name == "Dubai" && value > 57 || name == "Dubai" && value < -92 -> 30
-                name == "Moscow" && value > 57 || name == "Moscow" &&  value < -92 -> 5
-                name == "Hanoi" && value > 57 || name == "Hanoi" &&  value < -92 -> 20
-                else -> value
-            }
-        }
-}
-
-fun printColdestCity(list: MutableMap<Int, String>) {
-    val tempsAsKeys = list.keys.toList().sorted()
-    val isTempTheSame = tempsAsKeys[0] == tempsAsKeys[1]
-    if (isTempTheSame) {
-        print("neither")
-    } else {
-        print(list[tempsAsKeys[0]])
-    }
-}
+    var degrees: Int
+}        
 
 fun main() {
     val first = readLine()!!.toInt()
@@ -32,10 +14,5 @@ fun main() {
     thirdCity.degrees = third
 
     //implement comparing here
-    val cities= mutableMapOf(
-        secondCity.degrees to secondCity.name,
-        firstCity.degrees to firstCity.name,
-        thirdCity.degrees to thirdCity.name
-    )
-    printColdestCity(cities)
+    print()
 }
